2df0be8dc5e0dc7f68a0082bcf3d093ef68f17a7
DaryaBogdA <babaikabarabaika@yandex.by>
2024-10-14 16:28:13 +0300

    4

---
+++ 4.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+//{
+//    int num, res;
+//    cout << "Enter number: ";
+//    cin >> num;
+//    res = 0;
+//    while ( num > 0 )
+//    {
+//        num /= 10;
+//        res++;
+//    }
+//    cout << " number "<<res;
+//}
+
+
+
+//{
+//	int num, res, obratno;
+//	cout << "Enter number: ";
+//	cin >> num;
+//	obratno = 0;
+//	while (num > 0) {
+//		res = num % 10;
+//		obratno = obratno * 10 + res;
+//		num /= 10;
+//	}
+//	cout << " number "<< obratno;
+//}
+
+
+
+//{
+//	int num, chislo, res;
+//	cout << "Enter number: ";
+//	cin >> num;
+//	res = 0;
+//	while (num > 0) {
+//		chislo = num % 10;
+//		res = res + chislo;
+//		num /= 10;
+//	}
+//	cout << " number " << res;
+//}
