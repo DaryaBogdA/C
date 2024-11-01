ae74e4ba7dd9935accf138ade9f8d159bc3300d1
DaryaBogdA <babaikabarabaika@yandex.by>
2024-11-01 15:51:22 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -2,46 +2,52 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+void Task1()
+{
+    int num, res;
+    cout << "Enter number: ";
+    cin >> num;
+    res = 0;
+    do {
+        num /= 10;
+        res++;
+    } while (num > 0);
+    cout << " number " << res;
+}
+void Task2()
+{
+    int num, res, obratno;
+    cout << "Enter number: ";
+    cin >> num;
+    obratno = 0;
+    do {
+        res = num % 10;
+        obratno = obratno * 10 + res;
+        num /= 10;
+    } while (num > 0);
+    cout << " number " << obratno;
+}
+
+void Task3()
+{
+    int num, res, chislo;
+    cout << "Enter number: ";
+    cin >> num;
+    res = 0;
+    do {
+        {
+            chislo = num % 10;
+            res = res + chislo;
+            num /= 10;
+        }
+    } while (num > 0); 
+    cout << " number " << res;
+}
+
 int main()
-//{
-//    int num, res;
-//    cout << "Enter number: ";
-//    cin >> num;
-//    res = 0;
-//    while ( num > 0 )
-//    {
-//        num /= 10;
-//        res++;
-//    }
-//    cout << " number "<<res;
-//}
-
-
-
-//{
-//	int num, res, obratno;
-//	cout << "Enter number: ";
-//	cin >> num;
-//	obratno = 0;
-//	while (num > 0) {
-//		res = num % 10;
-//		obratno = obratno * 10 + res;
-//		num /= 10;
-//	}
-//	cout << " number "<< obratno;
-//}
-
-
-
-//{
-//	int num, chislo, res;
-//	cout << "Enter number: ";
-//	cin >> num;
-//	res = 0;
-//	while (num > 0) {
-//		chislo = num % 10;
-//		res = res + chislo;
-//		num /= 10;
-//	}
-//	cout << " number " << res;
-//}
+{
+    Task1();
+    Task2();
+    Task3();
+}
