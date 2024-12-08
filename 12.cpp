fbd551ae64cf80b5841a0b74af750dc69f5071e7
DaryaBogdA <babaikabarabaika@yandex.by>
2024-12-08 11:56:47 +0300

    Add files via upload

--- 12.cpp
+++ 12.cpp
@@ -46,7 +46,8 @@ void sort(int array[], int size) {
 //	}
 //}
 
-void associationArray(int* pindexarray1, int* pindexarray2, int* pindexarray3, int size) {
+int* associationArray(int* pindexarray1, int* pindexarray2, int size) {
+	int* pindexarray3 = new int[size];
 	for (int i = 0; i < size; i++) {
 		*pindexarray3 = *pindexarray1;
 		pindexarray3++;
@@ -56,21 +57,124 @@ void associationArray(int* pindexarray1, int* pindexarray2, int* pindexarray3, i
 		pindexarray1++;
 	}
 
+	return pindexarray3;
 }
-void associationArray2(int* pindexarray1, int* pindexarray2, int* pindexarray3, int size) {
-	for (int i = 0; i < size; i++) {
-		if (*pindexarray1 != *pindexarray2) {
-			*pindexarray3 = *pindexarray1;
-			pindexarray3++;
-			*pindexarray3 = *pindexarray2;
-			pindexarray3++;
-			pindexarray2++;
-			pindexarray1++;
 
-		}
-	}
 
+
+
+int size(const int* arr, int size) {
+    return size;
 }
+//все
+int* association(const int* arrA, int n, const int* arrB, int m) {
+    int* mergedArr = new int[n + m];
+    for (int i = 0; i < n; i++) {
+        mergedArr[i] = arrA[i];
+    }
+    for (int i = 0; i < m; i++) {
+        mergedArr[n + i] = arrB[i];
+    }
+    return mergedArr;
+}
+
+
+// общие
+int* common_elements(const int* arrA, const int* arrB, int n,  int m) {
+    int* common = new int[n];
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (arrA[i] == arrB[j]) {
+                common[count++] = arrA[i];
+                break;
+            }
+        }
+    }
+    int* result = new int[count];
+    for (int i = 0; i < count; i++) {
+        result[i] = common[i];
+    }
+    return result;
+}
+
+// эл не в В
+
+int* elements_not_in_B(const int* arrA, int n, const int* arrB, int m) {
+    int* result = new int[n];
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        bool found = false;
+        for (int j = 0; j < m; j++) {
+            if (arrA[i] == arrB[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            result[count++] = arrA[i];
+        }
+    }
+    int* finalResult = new int[count];
+    for (int i = 0; i < count; i++) {
+        finalResult[i] = result[i];
+    }
+    return finalResult;
+}
+
+// эл не в A
+int* elements_not_in_A(const int* arrA, int n, const int* arrB, int m) {
+    int* result = new int[m];
+    int count = 0;
+    for (int i = 0; i < m; i++) {
+        bool found = false;
+        for (int j = 0; j < n; j++) {
+            if (arrB[i] == arrA[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            result[count++] = arrB[i];
+        }
+    }
+    int* finalResult = new int[count];
+    for (int i = 0; i < count; i++) {
+        finalResult[i] = result[i];
+    }
+    return finalResult;
+}
+
+
+// обьед не общих
+int* unification_of_noncommon(const int* arrA, int n, const int* arrB, int m) {
+    int* elementsA = elements_not_in_B(arrA, n, arrB, m);
+    int countA = n;
+    int* elementsB = elements_not_in_A(arrA, n, arrB, m);
+    int countB = m;
+
+    int* result = new int[countA + countB];
+    for (int i = 0; i < countA; i++) {
+        result[i] = elementsA[i];
+    }
+
+    for (int i = 0; i < countB; i++) {
+        result[countA + i] = elementsB[i];
+    }
+
+    return result;
+}
+
+
+void result(const int* arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+
 int main()
 {
 	//1 
@@ -79,13 +183,11 @@ int main()
 	randomNum(array, m);
 	int* pOdd;
 	int* pEven;
-	for (int i = 0; i < m; i++) {
+	for (int i = 1; i < m; i++) {
 		if (array[i] % 2 != 0) {
-			if (i != 0) {
 				pOdd = &array[i];
 				pEven = &array[i - 1];
 				swap(pEven, pOdd);
-			}
 		}
 	}
 	result(array, m);*/
@@ -99,7 +201,6 @@ int main()
 	int *array2 = new int[m];
 	cout << " 2 array" << endl;
 	randomNum(array2, m);
-	int *array3 = new int[x];
 
 	cout << "1 sort" << endl;
 	sort(array1, n);
@@ -110,30 +211,53 @@ int main()
 	result(array2, m);
 
 	cout << "3 array" << endl;
-	associationArray(array1, array2, array3, x);
+	int *array3 = associationArray(array1, array2, x);
 	sort(array3, x);
 	result(array3, x);*/
 
 
 	//3
-	int m = 6, n = 7, x = m + n;
-	int* array1 = new int[n];
-	cout << "1 array" << endl;
-	randomNum(array1, n);
+    int n, m;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "Enter m: ";
+    cin >> m;
 
-	int* array2 = new int[m];
-	cout << " 2 array" << endl;
-	randomNum(array2, m);
-	int* array3 = new int[x];
+    int* arrA = new int[n];
+    int* arrB = new int[m];
+    cout << "Array A " << endl;
+    for (int i = 0; i < n; ++i) {
+        arrA[i] = rand() % 10;
+        cout << arrA[i] << " ";
+    }
+    cout << endl;
+    cout << "Array B " << endl;
+    for (int i = 0; i < m; ++i) {
+        arrB[i] = rand() % 10;
+        cout << arrB[i] << " ";
+    }
+    cout << endl;
 
-	cout << "3 array" << endl;
-	associationArray(array1, array2, array3, x);
-	sort(array3, x);
-	result(array3, x);
+    cout << "Association ";
+    int* paragraph1 = association(arrA, n, arrB, m);
+    result(paragraph1, n + m);
 
-	cout << "3 array 2" << endl;
-	associationArray2(array1, array2, array3, x);
-	sort(array3, x);
-	result(array3, x);
+    cout << "Common Elements ";
+    int* paragraph2 = common_elements(arrA, arrB, n, m);
+    result(paragraph2, size(paragraph2, min(n, m)));
+
+    cout << "Elements of A, not in B: ";
+    int* paragraph3 = elements_not_in_B(arrA, n, arrB, m);
+    result(paragraph3, size(paragraph3, n));
+
+    cout << "Elements of B, not in A: ";
+    int* paragraph4 = elements_not_in_A(arrA, n, arrB, m);
+    result(paragraph4, size(paragraph4, m));
+
+    cout << "Combining elements that are not common: ";
+    int* paragraph5 = unification_of_noncommon(arrA, n, arrB, m);
+    result(paragraph5, size(paragraph5, n + m));
 
 }
+
+
